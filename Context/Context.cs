e963347ed6f9de0b9acd40f4a12c5ea18993c863
Vitor <vtrpaulon@gmail.com>
2025-03-20 07:00:32 -0300

    Context

--- Context/Context.cs
+++ Context/Context.cs
@@ -2,6 +2,10 @@ namespace ProjetoMVC.Context
 {
     public class CadastroContext : DbContext
     {
+        public CadastroContext(DbContextOptions<CadastroContext> options) : base(options)
+        {
 
+        }
+        public DbSet<Cadastro> Cadastros { get; set; }
     }
 }
