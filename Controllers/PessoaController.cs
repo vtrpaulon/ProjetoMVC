ea8e5ea6218728fee5cb337fd64bb47dd0dcd083
Vitor <vtrpaulon@gmail.com>
2025-04-06 15:45:18 -0300

    criar

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
 
 namespace ProjetoMVC.Controllers
 {
     public class PessoaController : Controller
     {
+        private readonly CadastroContext _context;
+        public PessoaController(CadastroContext context)
+        {
+            _context = context;
+        }
         public ActionResult Index()
+        {
+            var pessoas = _context.Pessoas.ToList();
+            return View(pessoas);
+        }
+        public IActionResult Criar()
         {
             return View();
         }
